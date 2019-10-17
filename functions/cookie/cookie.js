f02ced278250a80e4f2d6ad3c9e295e9c04ccc96
Mark Tse <mark.tse@neverendingqs.com>
2019-10-16 20:37:15 -0400

    app - now trying with an external dep.

--- functions/cookie/cookie.js
+++ functions/cookie/cookie.js
@@ -1,10 +1,12 @@
+const cookie = require('cookie');
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
+    const cookies = cookie.parse('foo=bar; equation=E%3Dmc%5E2');
     const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` })
+      body: JSON.stringify({ message: `Hello ${subject}`, cookies })
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
