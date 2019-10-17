eccc12c61780d317c80d9218cb032af0acd4e916
Mark Tse <mark.tse@neverendingqs.com>
2019-10-16 20:57:17 -0400

    app - trying cookie manager code.

--- functions/cookie/cookie.js
+++ functions/cookie/cookie.js
@@ -1,17 +1,60 @@
 const cookie = require('cookie');
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+
+const cookieName = 'settings';
+
 exports.handler = async (event, context) => {
-  try {
-    const cookies = cookie.parse('foo=bar; equation=E%3Dmc%5E2');
-    const subject = event.queryStringParameters.name || 'World'
+  function tryParseJson(stringified) {
+    try {
+      return JSON.parse(stringified);
+    } catch {
+      return {};
+    }
+  }
+
+  function createCookie(cookieObj) {
+    return cookie.serialize(
+      cookieName,
+      JSON.stringify(cookieObj),
+      {
+        httpOnly: true,
+        path: '/',
+        maxAge: 60 * 60 * 24 * 14,
+        sameSite: 'strict',
+        secure: true
+      }
+    );
+  }
+
+  function createResponse(cookieObj) {
+    const cookieStr = createCookie(cookieObj);
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}`, cookies })
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      headers: {
+        'Set-Cookie': cookieStr,
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(cookieObj[cookieName]) || '{}'
+    };
+  }
+
+  try {
+    switch(event.httpMethod) {
+      case 'GET':
+        return createResponse(event.headers.cookie);
+      case 'POST':
+        return createResponse(tryParseJson(event.body));
+      default:
+        return {
+          statusCode: 501,
+          body: `HTTP method ${event.httpMethod} not supported.`
+          // // more keys you can return:
+          // headers: { "headerName": "headerValue", ... },
+          // isBase64Encoded: true,
+        };
     }
   } catch (err) {
-    return { statusCode: 500, body: err.toString() }
+    return { statusCode: 500, body: err.toString() };
   }
 }
