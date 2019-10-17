ab4622028cc36bdc16dd393d1c6830bf40017662
Mark Tse <mark.tse@neverendingqs.com>
2019-10-17 14:17:10 -0400

    Revert "app - test not using tryParseJson()."
    This reverts commit a8497a7a88d011f7409f73eb603b06d7fc190574.

--- functions/session/session.js
+++ functions/session/session.js
@@ -5,6 +5,14 @@ const cookie = require('cookie');
 
 const cookieName = 'session';
 
+function tryParseJson(stringified) {
+  try {
+    return JSON.parse(stringified);
+  } catch {
+    return {};
+  }
+}
+
 function createCookie(cookieStr) {
   return cookie.serialize(
     cookieName,
@@ -40,7 +48,7 @@ exports.handler = async (event, context) => {
             'Content-Type': 'application/json',
             'Set-Cookie': createCookie(body)
           },
-          body: JSON.stringify({ [cookieName]: JSON.parse(body) })
+          body: JSON.stringify({ [cookieName]: tryParseJson(body) })
         };
       default:
         return {
