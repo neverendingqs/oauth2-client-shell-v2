323821ab3a84ce65c44845186b03e0ed5ba9a7e5
Mark Tse <neverendingqs@users.noreply.github.com>
2019-10-17 14:43:48 -0400

    app - add cookie function (#1)
    * app - trying cookie manager code.
    
    * app - trying a simpler version.
    
    * app - now with a simple header.
    
    * app - adding cookie and cache header.
    
    * app - only taking the cookie we want.
    
    * app - use lodash.get() to handle undefined.
    
    * app - adding post.
    
    * app - renaming cookie to session.
    
    * app - cleaning up.
    
    * app - fixing up cookie string.
    
    * app - test not using tryParseJson().
    
    * Revert "app - test not using tryParseJson()."
    
    This reverts commit a8497a7a88d011f7409f73eb603b06d7fc190574.
    
    * app - trying err param on catch().
    
    * app - nit.
    
    * app - adding debugging.
    
    * app - adding a default for undefined.
    
    * app - removing unused dep.

---
+++ functions/session/session.js
@@ -0,0 +1,59 @@
+const cookie = require('cookie');
+
+const cookieName = 'session';
+
+function tryParseJson(stringified) {
+  try {
+    return JSON.parse(stringified);
+  } catch(err) {
+    return {};
+  }
+}
+
+function createCookie(cookieStr) {
+  return cookie.serialize(
+    cookieName,
+    cookieStr,
+    {
+      httpOnly: true,
+      path: '/',
+      maxAge: 60 * 60 * 24 * 14,
+      sameSite: 'strict',
+      secure: true
+    }
+  );
+}
+
+exports.handler = async (event, context) => {
+  const { body, headers, httpMethod } = event;
+  try {
+    switch(httpMethod) {
+      case 'GET':
+        return {
+          statusCode: 200,
+          headers: {
+            'Cache-Control': 'no-cache',
+            'Content-Type': 'application/json'
+          },
+          body: cookie.parse(headers.cookie || '')[cookieName] || '{}'
+        };
+      case 'POST':
+        return {
+          statusCode: 200,
+          headers: {
+            'Cache-Control': 'no-cache',
+            'Content-Type': 'application/json',
+            'Set-Cookie': createCookie(body)
+          },
+          body: JSON.stringify({ [cookieName]: tryParseJson(body) })
+        };
+      default:
+        return {
+          statusCode: 501,
+          body: `HTTP method ${httpMethod} not supported.`
+        };
+    }
+  } catch(err) {
+    return { statusCode: 500, body: err.toString() };
+  }
+}
